09708158a1fa5d10bafb735f15ee4b132e899a7b
Svjat <svjatderev@gmail.com>
2025-06-11 19:07:29 -0400

    Implemented code to start glew within the context of a sfml window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,65 @@
-#include <SFML/Graphics.hpp>
+#include <GL/glew.h>
+#include <SFML/Window.hpp>
 
-int main() {
+int main()
+{
+    int win_width = 800;
+    int win_height = 600;
+
+    sf::ContextSettings settings;
+    settings.majorVersion = 3;
+    settings.minorVersion = 3;
+    settings.depthBits = 24;
+
+    // create the window
+    sf::Window window(sf::VideoMode({(unsigned)win_width, (unsigned)win_height}), "OpenGL", sf::Style::Default, sf::State::Windowed, settings);
+    window.setVerticalSyncEnabled(true);
+
+    // activate the window
+    window.setActive();
+
+    // load glew AFTER context has been created
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // load resources, initialize the OpenGL states, ...
+
+    glViewport(0,0,win_width,win_height);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    while (window.isOpen()) {
+        // handle events
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {window.close();}
+            if (const auto* resized = event->getIf<sf::Event::Resized>()) {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, resized->size.x, resized->size.y);
+            }
+        }
+
+        // process input
+
+        // update
+
+        // clear the buffers
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw
+
+        // end the current frame (internally swaps the front and back buffers)
+        window.display();
+    }
+
+    // release resources...
+
     return 0;
 }
